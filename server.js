817665c9b9e4ac2df92c4a7946391785c4ccb69b
VS-2000 <vishnusuda32@gmail.com>
2026-01-08 21:49:34 +0530

    link edited

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "https://freelance-job-application-devlance.vercel.app/",
+  origin: "https://freelance-job-application-devlance.vercel.app",
   credentials: true,
 }));
 
