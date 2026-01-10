4d585a2f4547cf420090c951a62a9643380c84e1
VS-2000 <vishnusuda32@gmail.com>
2026-01-10 22:16:05 +0530

    admin only defined

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -49,4 +49,12 @@ const optionalProtect = async (req, res, next) => {
   }
 };
 
+const adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ message: "Not authorized as an admin" });
+  }
+};
+
 module.exports = { protect, adminOnly, optionalProtect };
